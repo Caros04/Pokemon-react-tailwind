b232fc904854a4f01217035cb22c8e3f88d18e2b
Julius Oscar <julius.oscar@banksinarmas.com>
2023-08-31 09:28:50 +0700

    first commit with filter and show sprite pokemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useMemo, useState } from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import About from './About';
 import './App.css';
+import Home from './Home';
+
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [text, setText] = useState('');
+  const [filterPokemon, setFilterPokemon] = useState([]);
+  useEffect(()=>{
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then((res)=>res.json())
+      .then((data)=>{
+        const results = data.results.map((pokemon, idx)=>{
+          return { ...pokemon, idx: idx+1};
+        });
+        setPokemon({ ...data, results});
+      });
+  })
+
+  useMemo(()=>{
+    if(text.length ===0){
+      setFilterPokemon([]); 
+      return;
+    }
+
+    setFilterPokemon(()=>
+      pokemon.results?.filter((pokemon)=>pokemon.name.includes(text))
+    )
+  },[pokemon.results, text])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='p-14'>
+        <div className='flex flex-col items-center'></div>
+          <Link to='/'>
+            <header className='text-4xl text-yellow-700'>Pokemon picker</header>
+
+          </Link>
+          <div className='w-full flex justify-center'>
+            <input 
+            type='text' 
+            onChange={($event) =>setText($event.target.value)}
+            placeholder='enter'
+            className='mt-10 p-2 border-blue-500 border-2'/>
+          </div>
+      </div>
+
+      <Routes>
+        <Route path='/about/:slug' element={<About/>} />
+        <Route path='/' element={pokemon && <Home pokemon = {filterPokemon}/>} />   
+      </Routes>
+    </Router>
+    
   );
 }
 
